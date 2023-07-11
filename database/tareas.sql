6aeebc9f700ec646ce801ed8bba488cd661dea1d
Sieghart205 <sieghart2005@gmail.com>
2023-07-11 18:26:23 -0500

    agregando base de datos

---
+++ database/tareas.sql
@@ -0,0 +1,16 @@
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+
+CREATE TABLE `tareas` (
+  `TareaID` int(11) NOT NULL,
+  `Tarea` text NOT NULL,
+  `Descripcion` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+
+ALTER TABLE `tareas`
+  ADD PRIMARY KEY (`TareaID`);
